1c7bdc0b717867312afe9c260c5c33b3d56faba5
peter oluwaseyi <peterseyi1010@gmail.com>
2025-11-09 13:08:40 +0100

    fix(Hero): changed hero image and edited padding and gaps

--- web/src/components/sections/core/HeroSection.tsx
+++ web/src/components/sections/core/HeroSection.tsx
@@ -5,8 +5,8 @@ import { ArrowUpRight } from "lucide-react"
 export default function HeroSection() {
   return (
     <section className="w-full bg-gradient-to-br from-primary/10 via-transparent to-primary/10">
-      <div className="max-w-7xl mx-auto px-6 py-20">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-1 md:gap-12 items-center">
+      <div className="max-w-7xl mx-auto px-6 py-6 md:py-30">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-12 items-center">
           {/* Image first on small screens, second on md+ */}
           <div className="flex items-center justify-center order-first md:order-2">
             <div className="relative w-full max-w-md">
@@ -14,7 +14,7 @@ export default function HeroSection() {
             </div>
           </div>
 
-          <div className="space-y-6 order-last md:order-1">
+          <div className="space-y-4 order-last md:order-1">
             <h1 className="text-4xl md:text-5xl font-extrabold leading-tight">
               Lifting Communities
               <br />
@@ -27,11 +27,11 @@ export default function HeroSection() {
             </p>
 
             <div className="flex flex-col sm:flex-row items-start sm:items-center gap-3">
-              <Button variant="default" className="cursor-pointer w-full md:w-auto">
+              <Button variant="default" className="cursor-pointer w-full px-8 md:w-auto">
                 Sponsor an Impact Today
                 <ArrowUpRight className="h-4 w-4" />
               </Button>
-              <Button variant="outline" className="cursor-pointer w-full md:w-auto">
+              <Button variant="outline" className="cursor-pointer w-full px-8 md:w-auto">
                 See Our Impact-Chain
               </Button>
             </div>
