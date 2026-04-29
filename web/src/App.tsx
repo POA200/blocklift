2215b2c190a5a7dd828a2441002da3961b1cc3c6
peter oluwaseyi <peterseyi1010@gmail.com>
2026-04-29 19:55:40 +0100

    vercel analytics2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,7 +14,6 @@ import Contact from "./pages/Contact";
 import Pay from "./pages/Pay";
 import Education from "./pages/Education";
 import Admin from "./pages/Admin";
-import { Analytics } from "@vercel/analytics/react";
 
 function ScrollToHash() {
   const { pathname, hash } = useLocation();
@@ -56,7 +55,6 @@ function App() {
           {/* fallback to home */}
           <Route path="*" element={<Home />} />
         </Routes>
-        <Analytics />
       </ThemeProvider>
     </>
   );
